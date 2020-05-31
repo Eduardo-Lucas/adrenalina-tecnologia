96ff1ef5f351f54a7b6a4e620818f3946ddb3b45
Eduardo Lucas <eduardolucas40@gmail.com>
2020-05-31 08:09:06 -0300

    Adicionado campo de foto nos Serviços

--- apps/core/models.py
+++ apps/core/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 class Servico(models.Model):
     nome = models.CharField(max_length=50, unique=True)
     descricao = models.CharField(max_length=200)
+    foto = models.ImageField(upload_to='images/servicos', blank=True, null=True)
 
     def __str__(self):
         return self.nome
