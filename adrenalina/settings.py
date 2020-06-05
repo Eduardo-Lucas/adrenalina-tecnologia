54a95158a89a0651ba578964866bea51430e0063
Eduardo Lucas <eduardolucas40@gmail.com>
2020-06-04 21:25:35 -0300

    Escondendo a SECRET_KEY da aplicação com o django-decouple

--- adrenalina/settings.py
+++ adrenalina/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6vjs=9=4+r4&sqiby(l)y9x-+x@q&$g7$g%o_z2zxxv#x)4g@c'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
