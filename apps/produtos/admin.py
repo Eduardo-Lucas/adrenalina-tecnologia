f8010c3928e163b076c1aa50d2162ac295e8e82a
Eduardo Lucas <eduardolucas40@gmail.com>
2020-06-15 20:37:06 -0300

    Permite importar-exportar a classe Produto

--- apps/produtos/admin.py
+++ apps/produtos/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 from apps.produtos.models import Produto
 
-admin.site.register(Produto)
+
+@admin.register(Produto)
+class ProdutoResource(ImportExportModelAdmin):
+    pass
